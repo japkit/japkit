70cc3a6eba81ef9bf381605cb2b4f63829ea3ebd
Stefan Ocke <stefanocke@users.noreply.github.com>
2014-04-14 20:28:10 +0200

    difftest3

--- japkit-roo-quickstart-example/src/main/java/de/stefanocke/japkit/roo/japkit/web/FormBackingObject.java
+++ japkit-roo-quickstart-example/src/main/java/de/stefanocke/japkit/roo/japkit/web/FormBackingObject.java
@@ -3,4 +3,6 @@ package de.stefanocke.japkit.roo.japkit.web;
 import de.stefanocke.japkit.metaannotations.classselectors.ClassSelector;
 
 @ClassSelector
-public abstract class FormBackingObject{}
+public abstract class FormBackingObject{
+	
+}
