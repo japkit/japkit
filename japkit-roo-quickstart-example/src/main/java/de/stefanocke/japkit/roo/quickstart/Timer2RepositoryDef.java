7a76f1f922256744c3f17cd1eec68f1a22839dba
Stefan Ocke <stefanocke@users.noreply.github.com>
2014-05-03 09:32:19 +0200

    removes Timer2 and 3, left only petclinic

--- japkit-roo-quickstart-example/src/main/java/de/stefanocke/japkit/roo/quickstart/Timer2RepositoryDef.java
+++
@@ -1,8 +0,0 @@
-package de.stefanocke.japkit.roo.quickstart;
-
-import de.stefanocke.japkit.roo.japkit.JapJpaRepository;
-
-@JapJpaRepository(domainType = Timer2.class)
-public interface Timer2RepositoryDef {
-
-}
