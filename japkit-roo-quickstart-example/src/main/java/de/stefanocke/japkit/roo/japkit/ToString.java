fa95ca48cab733e051d52a6de2da1a4026ade0ce
Stefan Ocke <stefanocke@users.noreply.github.com>
2014-07-13 15:17:32 +0200

    Fix for: renamed bodySwitch to bodyCases to avoid conflict with keyword

--- japkit-roo-quickstart-example/src/main/java/de/stefanocke/japkit/roo/japkit/ToString.java
+++ japkit-roo-quickstart-example/src/main/java/de/stefanocke/japkit/roo/japkit/ToString.java
@@ -4,14 +4,12 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import de.stefanocke.japkit.metaannotations.Case;
-import de.stefanocke.japkit.metaannotations.Constructor;
-import de.stefanocke.japkit.metaannotations.InnerClass;
 import de.stefanocke.japkit.metaannotations.Matcher;
-import de.stefanocke.japkit.metaannotations.TypeCategory;
-import de.stefanocke.japkit.metaannotations.Var;
 import de.stefanocke.japkit.metaannotations.Method;
 import de.stefanocke.japkit.metaannotations.Properties;
 import de.stefanocke.japkit.metaannotations.Template;
+import de.stefanocke.japkit.metaannotations.TypeCategory;
+import de.stefanocke.japkit.metaannotations.Var;
 import de.stefanocke.japkit.metaannotations.classselectors.GeneratedClass;
 
 @Template(vars = @Var(name = "toStringProperties", propertyFilter = @Properties(sourceClass = GeneratedClass.class)))
@@ -22,7 +20,7 @@ public abstract class ToString {
 			//bodyIteratorVar="p"
 			bodyBeforeExpr = "return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).\n",
 			
-			bodySwitch={
+			bodyCases={
 				//Only summary for collections
 				@Case(matcher=@Matcher(srcTypeCategory=TypeCategory.COLLECTION), 
 						expr = "append(\"#{element.simpleName}\", #{element.getter.simpleName}(), false).\n"),
@@ -33,15 +31,4 @@ public abstract class ToString {
 			)
 	public abstract String toString();
 	
-//	@Constructor(bodyExpr="//Some ctor code")
-//	public ToString (){};
-	
-	//TODO: Take name from class if no nameExpr is given...  
-	//TODO: Consider the inner class as a template for them members of the inner class to be generated.
-	//TODO: rule src element for name expression should not always be the annotated class.
-//	@InnerClass(iterator="#{toStringProperties}", nameExpr="#{parent.element.simpleName.toFirstUpper}InnerClassTest")
-//	static class InnerClassTest{
-//		@Method
-//		void foo(){}
-//	}
 }
