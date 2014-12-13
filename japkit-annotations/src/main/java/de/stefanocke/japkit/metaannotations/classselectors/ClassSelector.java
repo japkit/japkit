17f92431cd1e22e9b79299f1989f01f8ce8f0c90
Stefan Ocke <stefanocke@users.noreply.github.com>
2014-12-13 11:57:30 +0100

    removed avName in ClassSelector

--- japkit-annotations/src/main/java/de/stefanocke/japkit/metaannotations/classselectors/ClassSelector.java
+++ japkit-annotations/src/main/java/de/stefanocke/japkit/metaannotations/classselectors/ClassSelector.java
@@ -14,25 +14,6 @@ public @interface ClassSelector {
 
 	/**
 	 * 
-	 * @return the name of the annotation value of the trigger annotation.
-	 *         <p>
-	 *         In case of ClassSelectorKind.TYPE_MIRROR that annotation value
-	 *         must be of type Class<?>.
-	 *         <p>
-	 *         In case of ClassSelectorKind.INNER_CLASS_NAME it must be of type
-	 *         String.
-	 *         <p>
-	 *         In case of ClassSelectorKind.SPECIAL it depends on the selector.
-	 *         Usually, it is ignored at all.
-	 *         <p>
-	 *         If empty, the annotation value name defaults to the simple class
-	 *         name of the class annotated with ClassSelector with first
-	 *         character converted to lower case.
-	 */
-	String avName() default "";
-
-	/**
-	 * 
 	 * @return for {@link ClassSelectorKind#EXPR} and as fallback for
 	 *         TYPE_MIRROR and INNER_CLASS_NAME, if the AV does not exist.
 	 */
