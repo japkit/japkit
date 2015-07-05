6a481bbf09ad283cce6a719eaed29d8ebd7c70cc
Stefan Ocke <stefanocke@users.noreply.github.com>
2015-07-05 11:04:56 +0200

    minor example improvements

--- japkit-roo-quickstart-example/src/main/java/de/japkit/roo/japkit/CommonLibrary.java
+++ japkit-roo-quickstart-example/src/main/java/de/japkit/roo/japkit/CommonLibrary.java
@@ -3,6 +3,11 @@ package de.japkit.roo.japkit;
 import de.japkit.metaannotations.Function;
 import de.japkit.metaannotations.Library;
 
+/**
+ * TODO: Check whether to move to annotation processor.
+ * @author stefan
+ *
+ */
 @Library
 public class CommonLibrary {
 
@@ -12,12 +17,12 @@ public class CommonLibrary {
 	@Function(expr="#{src.simpleName.toFirstUpper}")
 	public class nameFirstUpper{}
 	
+	/**If src is a Map entry, this function retrieves its key.*/
 	@Function(expr="#{src.key}")
-	public class key{}
+	public class SrcKey{}
 	
+	/**If src is a Map entry, this function retrieves its value.*/
 	@Function(expr="#{src.value}")
-	public class value{}
+	public class SrcValue{}
 	
-	@Function(expr="#{src.key.type}")
-	public class SrcKeyType{}
 }
--- japkit-roo-quickstart-example/src/main/java/de/japkit/roo/japkit/web/ControllerMembersJpaRepository.java
+++ japkit-roo-quickstart-example/src/main/java/de/japkit/roo/japkit/web/ControllerMembersJpaRepository.java
@@ -14,9 +14,9 @@ import de.japkit.metaannotations.classselectors.SrcSingleValueType;
 import de.japkit.roo.base.web.CrudOperations;
 import de.japkit.roo.base.web.RepositoryAdapter;
 import de.japkit.roo.japkit.CommonLibrary;
-import de.japkit.roo.japkit.CommonLibrary.key;
+import de.japkit.roo.japkit.CommonLibrary.SrcKey;
 import de.japkit.roo.japkit.CommonLibrary.nameFirstLower;
-import de.japkit.roo.japkit.CommonLibrary.value;
+import de.japkit.roo.japkit.CommonLibrary.SrcValue;
 import de.japkit.roo.japkit.domain.DomainLibrary.findRepository;
 import de.japkit.roo.japkit.domain.DomainLibrary.isEntity;
 import de.japkit.roo.japkit.web.JapkitWebScaffold.Repository;
@@ -32,18 +32,18 @@ public abstract class ControllerMembersJpaRepository {
 	@Var(expr = "#{viewProperties}", filterFun = isEntity.class)
 	class entityProperties{}
 	
-	@Template(srcFun=entityProperties.class, srcGroupBy="#{src.singleValueType}", srcGroupByFun=findRepository.class ) 
+	@Template(srcFun=entityProperties.class, srcGroupByFun={SrcSingleValueType.class, findRepository.class} ) 
 	abstract class RelatedEntityMembers{
 		
-		@Var(fun={key.class, nameFirstLower.class})
+		@Var(fun={SrcKey.class, nameFirstLower.class})
 		class repositoryFieldName {}
 		
 		@Field
 		@Autowired
-		private key $repositoryFieldName$;
+		private SrcKey $repositoryFieldName$;
 		
 		
-		@Method(srcFun=value.class, bodyCode = "return #{repositoryFieldName}.findAll();")
+		@Method(srcFun=SrcValue.class, bodyCode = "return #{repositoryFieldName}.findAll();")
 		protected abstract List<SrcSingleValueType> get$srcElementName$Choices();
 	}
 
@@ -51,7 +51,6 @@ public abstract class ControllerMembersJpaRepository {
 			bodyCode = "return new RepositoryAdapter<#{fbo.code}>(repository);")
 	protected abstract CrudOperations<FormBackingObject> crudOperations();
 
-	//TODO: Eigentlich singleValueType.
 	/**
 	 * @japkit.bodyCode <code>uiModel.addAttribute("${src.name}Choices", get${src.name.toFirstUpper}Choices());</code>
 	 */
