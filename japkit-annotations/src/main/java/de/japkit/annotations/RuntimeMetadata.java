25bd2422be68854180116c3549f710f7143dfed0
Stefan Ocke <stefanocke@users.noreply.github.com>
2016-07-17 14:22:42 +0200

    Workaround for Eclipse Luna bug:  The class methodOrConstructor has no annotations (probably since it does not get resolved by jdt apt)

--- japkit-annotations/src/main/java/de/japkit/annotations/RuntimeMetadata.java
+++ japkit-annotations/src/main/java/de/japkit/annotations/RuntimeMetadata.java
@@ -1,8 +1,5 @@
 package de.japkit.annotations;
 
-import static javax.lang.model.element.ElementKind.CONSTRUCTOR;
-import static javax.lang.model.element.ElementKind.METHOD;
-
 import de.japkit.annotations.RuntimeMetadata.Element;
 import de.japkit.annotations.RuntimeMetadata.List;
 import de.japkit.metaannotations.AV;
@@ -10,7 +7,6 @@ import de.japkit.metaannotations.AVMode;
 import de.japkit.metaannotations.Annotation;
 import de.japkit.metaannotations.AnnotationMode;
 import de.japkit.metaannotations.Clazz;
-import de.japkit.metaannotations.Matcher;
 import de.japkit.metaannotations.Trigger;
 import de.japkit.metaannotations.Var;
 
@@ -20,16 +16,13 @@ import de.japkit.metaannotations.Var;
 			values={
 				@AV(name="id", expr="#{elements.uniqueNameWithinTopLevelEnclosingTypeElement(src)}"), 
 				@AV(name="comment", expr="#{elements.getDocComment(src)}"),
-				@AV(condFun=RuntimeMetadata.methodOrConstructor.class, name="paramNames", expr="#{src.parameters}")
+				@AV(cond="#{src.kind.name()=='METHOD' || src.kind.name()=='CONSTRUCTOR'}", name="paramNames", expr="#{src.parameters}")
 		}),
 	@Annotation(targetAnnotation=List.class, mode=AnnotationMode.MERGE, 
 		values=@AV(name = "value", expr="#{elements.elementAndAllEnclosedElements(src)}", mode=AVMode.JOIN_LIST, annotationMappingId="elementMetadata"))
 })
 
 public @interface RuntimeMetadata {
-	@Matcher(kind={METHOD, CONSTRUCTOR})
-	class methodOrConstructor{}
-	
 	public static final String CLASS_SUFFIX = "_RuntimeMetadata";
 	
 	boolean shadow() default false;
