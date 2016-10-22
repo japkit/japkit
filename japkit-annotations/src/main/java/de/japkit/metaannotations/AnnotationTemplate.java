7c44511e538629fddad3b702956497f206662b21
Stefan Ocke <stefanocke@users.noreply.github.com>
2016-10-22 14:49:55 +0200

    Moved Annotation Templates from samples to japkit.

---
+++ japkit-annotations/src/main/java/de/japkit/metaannotations/AnnotationTemplate.java
@@ -0,0 +1,12 @@
+package de.japkit.metaannotations;
+
+import java.lang.annotation.Annotation;
+
+/** Marks an annotation as a template for generating another annotation. */
+public @interface AnnotationTemplate {
+	/**
+	 * 
+	 * @return the annotation to be generated.
+	 */
+	Class<? extends Annotation> targetAnnotation();
+}
---
+++ japkit-annotationtemplates/src/main/java/de/japkit/annotationtemplates/AnnotationTemplatesTemplate.java
@@ -0,0 +1,93 @@
+package de.japkit.annotationtemplates;
+
+import static de.japkit.metaannotations.classselectors.ClassSelectorKind.INNER_CLASS_NAME;
+
+import javax.lang.model.element.ElementKind;
+
+import de.japkit.annotations.RuntimeMetadata;
+import de.japkit.annotationtemplates.AnnotationTemplatesTemplate.AnnotationName_.AnnotationTemplateAVMembers;
+import de.japkit.metaannotations.Clazz;
+import de.japkit.metaannotations.Matcher;
+import de.japkit.metaannotations.Method;
+import de.japkit.metaannotations.Template;
+import de.japkit.metaannotations.TemplateCall;
+import de.japkit.metaannotations.classselectors.ClassSelector;
+import de.japkit.metaannotations.classselectors.SrcSingleValueType;
+import de.japkit.metaannotations.classselectors.SrcType;
+
+@RuntimeMetadata
+@Clazz()
+public class AnnotationTemplatesTemplate {
+	@de.japkit.metaannotations.InnerClass(src = "#{triggerAnnotation.targetAnnotations}",
+			nameExpr = "#{src.asElement().simpleName}_",
+			kind = ElementKind.ANNOTATION_TYPE,
+			templates = @TemplateCall(AnnotationTemplateAVMembers.class))
+	@de.japkit.metaannotations.AnnotationTemplate(targetAnnotation = SrcType.class)
+	public @interface AnnotationName_ {
+		
+		/**
+		 * @return expression to determine if the annotation shall be generated. Default is true.
+		 */
+		String _cond() default "";
+		
+		/** 
+		 * @return the expression language for the condition expression.
+		 */
+		String _condLang() default "";
+		
+		/** 
+		 * @return as an alternative to the cond expression, a boolean function can be called.
+		 */
+		Class<?>[] _condFun() default {};
+		
+		@Template(src = "#{elements.declaredMethods(src.asElement())}")
+		public @interface AnnotationTemplateAVMembers {
+			
+			@Method
+			SrcSingleValueType[]  $srcElementName$() default {};
+			
+			@Matcher(condition="#{src.singleValueType.kind == 'DECLARED' && src.singleValueType.asElement.kind == 'ANNOTATION_TYPE'}")
+			class isAnnotationType{};
+			
+			/**
+			 * #{src}
+			 * #{src.returnType}
+			 * #{src.singleValueType.asElement.simpleName}
+			 * #{src.singleValueType.kind == 'DECLARED' && src.singleValueType.asElement.kind == 'ANNOTATION_TYPE'}
+			 * */
+			@Method(condFun = isAnnotationType.class)
+			AnnotationTemplateType[] _$srcElementName$() default {}; 
+			
+			//TODOs: 
+			//UnresolvedType per Flag ermöglichen, damit Abhängigkeiten zwischen inner classes "von Hand" aufgelöst werden können.
+			//Activation ergänzen: Nur generieren , wenn der Typ in der Trigger Annotation auftaucht.
+			@ClassSelector(kind=INNER_CLASS_NAME, expr="#{src.singleValueType.asElement.simpleName}_", enclosing=GenClassEnclosingClass.class)
+			@interface AnnotationTemplateType{}
+			
+			@ClassSelector(expr="#{genClass.enclosingElement.asType()}")
+			@interface GenClassEnclosingClass{}
+
+			/**
+			 * @return expression to determine if the annotation value shall be generated. Default is true.
+			 */
+			String _$srcElementName$Cond() default "";
+			
+			/** 
+			 * @return the expression language for the condition expression.
+			 */
+			String _$srcElementName$CondLang() default "";
+			
+			/** 
+			 * @return as an alternative to the cond expression, a boolean function can be called.
+			 */
+			Class<?>[] _$srcElementName$CondFun() default {};
+
+			///??? Was macht das bei einfachen annotation values???
+			String _$srcElementName$Src() default "";
+
+			String _$srcElementName$Expr() default "";
+
+		}
+
+	}
+}
