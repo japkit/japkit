fa6d0ce1b8345e3912861f4a7be9f3408cea53a0
Stefan Ocke <stefanocke@users.noreply.github.com>
2017-01-29 19:17:04 +0100

    improved javadoc in annotation templates

--- japkit-annotationtemplates/src/main/java/de/japkit/annotationtemplates/AnnotationTemplatesTemplate.java
+++ japkit-annotationtemplates/src/main/java/de/japkit/annotationtemplates/AnnotationTemplatesTemplate.java
@@ -15,12 +15,18 @@ import de.japkit.metaannotations.Matcher;
 import de.japkit.metaannotations.Method;
 import de.japkit.metaannotations.Template;
 import de.japkit.metaannotations.TemplateCall;
+import de.japkit.metaannotations.Var;
 import de.japkit.metaannotations.classselectors.ClassSelector;
 
 @RuntimeMetadata
 @Clazz()
 public class AnnotationTemplatesTemplate {
+	/**
+	 * This annotation template will generate an annotation of type {@link #{src.qualifiedName}}. 
+	 *
+	 */
 	@de.japkit.metaannotations.InnerClass(src = "#{triggerAnnotation.targetAnnotations}",
+			vars=@Var(name="targetAnnotation", expr="#{src}"),
 			nameExpr = "#{src.asElement().simpleName}_",
 			kind = ElementKind.ANNOTATION_TYPE,
 			templates = @TemplateCall(AnnotationTemplateAVMembers.class),
@@ -29,7 +35,6 @@ public class AnnotationTemplatesTemplate {
 	//@de.japkit.annotations.AnnotationTemplate(targetAnnotation = SrcType.class)
 	public @interface AnnotationName_ {
 		
-
 		/**
 		 * An expression to determine the source object for generating this annotation(s).
 		 * The source element is available as "src" in expressions and is used in
@@ -69,15 +74,22 @@ public class AnnotationTemplatesTemplate {
 		
 		@Template(src = "#{elements.declaredMethods(src.asElement())}")
 		public @interface AnnotationTemplateAVMembers {
-			
+			/**
+			 * @return a constant value for annotation value '#{name}'.
+			 * @see #{targetAnnotation.qualifiedName}#value()
+			 */
 			@Method
-			SrcSingleValueType[]  $srcElementName$() default {};
+			SrcSingleValueType[]  $name$() default {};
 			
 			@Matcher(condition="#{src.singleValueType.kind == 'DECLARED' && src.singleValueType.asElement.kind == 'ANNOTATION_TYPE'}")
 			class isAnnotationType{};
 			
+			/**
+			 * @return the annotation template to generate the value of annotation value '#{name}'. 
+			 * 	Can be used as alternative to {@link #{name}()} if the value is not constant.
+			 */
 			@Method(condFun = isAnnotationType.class)
-			AnnotationTemplateType[] $srcElementName$_() default {}; 
+			AnnotationTemplateType[] $name$_() default {}; 
 			
 			@ClassSelector(kind=INNER_CLASS_NAME, expr="#{src.singleValueType.asElement.simpleName}_", enclosing=GenClassEnclosingClass.class)
 			@interface AnnotationTemplateType{}
@@ -86,24 +98,33 @@ public class AnnotationTemplatesTemplate {
 			@interface GenClassEnclosingClass{}
 
 			/**
-			 * @return expression to determine if the annotation value shall be generated. Default is true.
+			 * @return expression to determine if the annotation value '#{name}' shall be generated. Default is true.
 			 */
-			String $srcElementName$_cond() default "";
+			String $name$_cond() default "";
 			
 			/** 
-			 * @return the expression language for the condition expression.
+			 * @return the expression language for the condition expression for '#{name}'.
 			 */
-			String $srcElementName$_condLang() default "";
+			String $name$_condLang() default "";
 			
 			/** 
-			 * @return as an alternative to the cond expression, a boolean function can be called.
+			 * @return as an alternative to the cond expression, a boolean function can be called to determine if the annotation value '#{name}' shall be generated.
 			 */
-			Class<?>[] $srcElementName$_condFun() default {};
+			Class<?>[] $name$_condFun() default {};
 
+			/**
+			 * 
+			 * @return the src expression for generating annotation value '#{name}'. If this results in a collection, an array will be generated.
+			 */
 			///??? Was macht das bei einfachen annotation values???
-			String $srcElementName$_src() default "";
+			String $name$_src() default "";
 
-			String $srcElementName$_expr() default "";
+			/**
+			 * 
+			 * @return the expression for the value of annotation value '#{name}'. 
+			 * Can be used as alternative to {@link #{name}()} if the value is not constant.
+			 */
+			String $name$_expr() default "";
 
 		}
 
